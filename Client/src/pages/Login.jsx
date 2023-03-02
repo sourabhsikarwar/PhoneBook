964fe3248ed0c798cee15965d280319d43e241fc
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2023-03-03 00:06:00 +0530

    Client env setup

---
+++ Client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Contact from '../components/Contact'
+
+const Login = () => {
+  return (
+    <div className='text-xl text-blue-600 font-bold'>Login</div>
+  )
+}
+
+export default Login
